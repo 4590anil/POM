3f38dabfa5ea7311e57b09659d618b4e0b149825
anirawat1 <anirawat1@WKWIN9921138.global.publicisgroupe.net>
2018-05-29 11:45:37 +0530

    Removed ITest Result

--- src/main/java/PageObjects/SapientHomePage.java
+++ src/main/java/PageObjects/SapientHomePage.java
@@ -24,7 +24,7 @@ public class SapientHomePage {
 
 	@FindBy(xpath = "//div[@class='PageContainer']//div[@class='titleComp titlecomp']/h2")
 	WebElement AboutText;
-
+/*
 	public void AboutDetails(ITestResult result) throws Exception {
 
 		try {
@@ -37,6 +37,27 @@ public class SapientHomePage {
 			String screenshotPath = ExtentReport.getScreenshot(driver, result.getName());
 			 //To add it in the extent report 
 			ExtentReport.logger.log(LogStatus.FAIL, ExtentReport.logger.addScreenCapture(screenshotPath));
+		}
+		ExtentReport.logger.log(LogStatus.INFO, "Landed on About Page");
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+
+		ExtentReport.logger.log(LogStatus.INFO, "Information in About Page - " + AboutText.getText());
+		ExtentReport.logger.log(LogStatus.PASS, "Successfully fetched info from About Page of Sapient");
+	}
+	*/
+	
+	public void AboutDetails()  {
+
+		try {
+			
+		About.click();
+		ExtentReport.logger.log(LogStatus.PASS, "Successfully clicked on About on Sapient homepage");
+		}
+		catch (Exception e) {
+			ExtentReport.logger.log(LogStatus.FAIL, "Failed on clicking the about tab - "+e);
+	
+			 //To add it in the extent report 
+
 		}
 		ExtentReport.logger.log(LogStatus.INFO, "Landed on About Page");
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
